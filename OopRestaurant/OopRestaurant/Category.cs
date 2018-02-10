2106bf8e5651764f07788bfaa73957a09c642266
csaszariz <zoltan.csaszari@borsodchem.ez>
2018-02-10 15:05:19 +0100

    #5:  Category létrehozása

---
+++ OopRestaurant/OopRestaurant/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace OopRestaurant
+{
+    /// <summary>
+    /// Az ételek kategóriáját tartalmazó osztály
+    /// </summary>
+    public class Category
+    {
+        /// <summary>
+        /// Az adatbázisba íráshoz kell egy primary key property a CodeFirst névkonvenció alapján ennek neve "Key" vagy "Id"
+        /// Ezt felismeri és az adatbázisban ennek megfelelően hozza létre aaz adattáblákat
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Az ételkategória neve
+        /// </summary>
+        public string Name{ get; set; }
+    }
+}
