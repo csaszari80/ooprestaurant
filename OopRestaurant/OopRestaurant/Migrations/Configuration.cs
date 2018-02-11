acfbe8de25e538d24634d2bdc923557157cf4e5a
csaszariz <zoltan.csaszari@borsodchem.ez>
2018-02-11 18:03:29 +0100

    #6: Seed függvény kijavítása (SaveChanges())

--- OopRestaurant/OopRestaurant/Migrations/Configuration.cs
+++ OopRestaurant/OopRestaurant/Migrations/Configuration.cs
@@ -35,11 +35,12 @@ namespace OopRestaurant.Migrations
                 new Category( name : "Italok"),
                 new Category( name : "Desszettek")
             );
+            context.SaveChanges();
 
             /// mivel a következõ lépésben szükség lesz a létrejött Categóriák teljes modeljére (Id-vel együtt)
-            /// ezért lekérdezzük a .Single metódus akkor is egy eredményt ad vissza ha több is elõfordul a lambda kifejezésben megadott mezõre vonatkozó feltétel alapján.
-            var pizzaCategory = context.Categories.Single(x => x.Name == "Pizzák");
-            var drinkCategory = context.Categories.Single(x => x.Name == "Italok");
+            /// ezért lekérdezzük a .Single metódus egy eredményelemet ad vissza akkor is ha több is elõfordul a lambda kifejezésben megadott mezõre vonatkozó feltétel alapján.
+            Category pizzaCategory = context.Categories.Single(x => x.Name == "Pizzák");
+            Category drinkCategory = context.Categories.Single(x => x.Name == "Italok");
 
             /// Feltöltjük a MenuItems táblát is néhány adattal felhasználva az elõzõ lépésben lekérdezett kategóriákat
             context.MenuItems.AddOrUpdate(
@@ -48,7 +49,8 @@ namespace OopRestaurant.Migrations
                 new MenuItem(name: "Hawai", description: "sonka, ananász, mozzarella, pizzaszósz", price: 300, category: pizzaCategory),
                 new MenuItem(name: "Cola", description: "3 dl", price: 120, category: drinkCategory),
                 new MenuItem(name: "Ásványvíz", description: "3 dl", price: 100, category: drinkCategory)
-            );  
+            );
+            context.SaveChanges();
         }
     }
 }
